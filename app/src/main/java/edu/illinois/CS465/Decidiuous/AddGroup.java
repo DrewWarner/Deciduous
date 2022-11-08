6afce8a55254d7b836171a60c9cfb4d561554d6e
werdwar <werdwar@gmail.com>
2022-11-08 15:22:33 -0600

    added a new activity (which includes connection to the manifest) for the add a group activity. This will need fixed as it has more strict requirements than regular xml layouts. Also added save/load to main activity as well as parcelable implementation to custom groupinfo class.

--- app/src/main/java/edu/illinois/CS465/Decidiuous/AddGroup.java
+++ app/src/main/java/edu/illinois/CS465/Decidiuous/AddGroup.java
@@ -1,13 +1,14 @@
 package edu.illinois.CS465.Decidiuous;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateNewActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class AddGroup extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_group_layout);
+        setContentView(R.layout.activity_add_group);
     }
 }
